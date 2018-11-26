036c654a120577269ecbbd7f826aac0310d4ca51
tanzongxi <tanzongxi@ifunq.com>
2018-11-26 19:29:25 +0800

    [ADD] Java8 了解

---
+++ git-maven-module/src/main/java8/org/ifunq/tanzongxi/lambda/LambdaTest05.java
@@ -0,0 +1,28 @@
+package org.ifunq.tanzongxi.lambda;
+
+import java.util.function.Consumer;
+
+public class LambdaTest05 {
+
+    public static void main(String[] args) {
+        // lambda定义有无回值，编译通过
+        Consumer consumer1 = (msg) -> System.out.println(msg);
+        // lambda定义有返回值，编译错误
+        // Consumer consumer2 = (msg) -> {System.out.println(msg); return msg;};
+        // 引用带无返回值的方法引用
+        Consumer<String> consumer3 = LambdaTest05::sayNoResult;
+        // 引用带有返回值的方法引用，但是返回值时获取不到的，因为Consumer.accept方法定义无返回值
+        Consumer<String> consumer4 = LambdaTest05::sayWithResult;
+        consumer1.accept("Steven");
+        consumer3.accept("Anna");
+        consumer4.accept("Jack");
+    }
+
+    public static void sayNoResult(String msg) {
+        System.out.println(msg);
+    }
+    public static String sayWithResult(String msg) {
+        System.out.println(msg);
+        return msg;
+    }
+}
---
+++ git-maven-module/src/main/java8/org/ifunq/tanzongxi/lambda/LambdaTest07.java
@@ -0,0 +1,28 @@
+package org.ifunq.tanzongxi.lambda;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LambdaTest07 {
+
+    public static void main(String[] args) {
+        List<String> strings = Arrays.asList("abc", "", "bc", "efg", "abcd","", "jkl", "",
+                "sfasf", "ff", "gg", "33", "44", "66", "jj", "sdf", "123", "fdhg", "4234");
+        List<String> filtered = strings
+                // 准备并行Stream
+                .parallelStream()
+                // .stream()
+                // 过滤掉为空数据
+                .filter(string -> {System.out.println(string);  return  !string.isEmpty();})
+                // 过滤掉长度大于3的数据
+                .filter(str -> str.length() <= 3)
+                // 取前10条数据
+                .limit(10)
+                // 组成新的数组
+                .collect(Collectors.toList());
+        System.out.println(filtered);
+
+//        filtered.stream().max();
+    }
+}
